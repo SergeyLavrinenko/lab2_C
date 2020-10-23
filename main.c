66c57905f5a9ea263de85d9b0e7c12e7dba64136
SergeyLavrinenko <l.s.a00@mail.ru>
2020-10-24 02:00:19 +0700

    Добавлены математические функции

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 # include <stdio.h>
 
-int main(void){
-	printf("\nhello world\n\n");
-	return 0;
+float summ(float a, float b){
+        printf("Сумма чисел %g и %g равна %g\n", a, b, a + b);
+        return a + b;
 }
 
+float multipl(float a, float b){
+        printf("Произведение чисел %g и %g равно %g\n", a, b, a * b);
+        return a * b;
+}
+
+float subtract(float a, float b){
+        printf("Результат вычитания числа %g из %g равен %g\n", b, a, a - b);
+        return a - b;
+}
+
+float division(float a, float b){
+        printf("Частное чисел %g и %g равно %g\n", a, b, a / b);
+        return a / b;
+}
+
+
+
+int main(void){
+	printf("\nhello world\n\n");
+	summ(3, multipl(3, -2));
+	subtract(4, division(3, -2));
+	return 0;
+}
