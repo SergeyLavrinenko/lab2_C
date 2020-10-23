36dc9d19d1a674f7f0ced79005c688a7af2231bd
SergeyLavrinenko <l.s.a00@mail.ru>
2020-10-24 02:06:35 +0700

    *

--- main.c
+++ main.c
@@ -5,6 +5,11 @@ float summ(float a, float b){
         return a + b;
 }
 
+float multipl(float a, float b){
+        printf("Произведение чисел %g и %g равно %g\n", a, b, a * b);
+        return a * b;
+}
+
 int main(void){
 	printf("\nhello world\n\n");
 	return 0;
